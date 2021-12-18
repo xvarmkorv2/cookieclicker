05dd1f2726c2d456411c595c15b578cffe55f242
xvarmkorv2 <49081442+xvarmkorv2@users.noreply.github.com>
2021-12-18 14:55:42 -0500

    heroku

--- patreon/grab.php
+++ patreon/grab.php
@@ -1,35 +1,22 @@
 <?php
 header("Access-Control-Allow-Origin: https://xvarmkorv2.github.io");
-echo "why:";
-//extract data from the post
-extract($_GET);
-
 //set POST variables
 $url = 'https://orteil.dashnet.org/patreon/grab.php';
 $fields_string = "";
-$fields = array(
-        'lname'=>urlencode($last_name), // Assuming there was something like $_POST[last_name]
-        'fname'=>urlencode($first_name)
-    );
-
-//url-ify the data for the POST
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+/url-ify the data for the POST
+foreach($_GET as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 $fields_string = rtrim($fields_string,'&');
 
 //open connection
 $ch = curl_init();
+$headers = getallheaders()
 
 //set the url, number of POST vars, POST data
 curl_setopt($ch,CURLOPT_URL,$url.$fields_string);
 curl_setopt($ch,CURLOPT_HTTPGET,true);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch,CURLOPT_HTTPHEADER,array (
-    "Accept: application/json"
-));
-curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"]);
-foreach (getallheaders() as $name => $value) {
-    echo "$name: $value <br>";
-}
+curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+#curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"]);
 
 //execute post
 $result = curl_exec($ch);
@@ -37,6 +24,5 @@ $result = curl_exec($ch);
 //close connection
 curl_close($ch);
 echo $result;
-echo ":why";
 exit;
 ?>

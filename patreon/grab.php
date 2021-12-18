7972b4e7cc5c4e423215d10e912d214d3126b959
xvarmkorv2 <49081442+xvarmkorv2@users.noreply.github.com>
2021-12-18 15:01:54 -0500

    hekoku

--- patreon/grab.php
+++ patreon/grab.php
@@ -1,28 +1,44 @@
 <?php
-header("Access-Control-Allow-Origin: https://xvarmkorv2.github.io");
-//set POST variables
+/*
+* Author: Ojas Ojasvi
+* Released: September 25, 2007
+* Description: An example of the disguise_curl() function in order to grab contents from a website while remaining fully camouflaged by using a fake user agent and fake headers.
+*/
+
 $url = 'https://orteil.dashnet.org/patreon/grab.php';
-$fields_string = "";
-//url-ify the data for the POST
-foreach($_GET as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-$fields_string = rtrim($fields_string,'&');
 
-//open connection
-$ch = curl_init();
-$headers = getallheaders();
+// disguises the curl using fake headers and a fake user agent.
+function disguise_curl($url)
+{
+  $curl = curl_init();
 
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL,$url.$fields_string);
-curl_setopt($ch,CURLOPT_HTTPGET,true);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
-#curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"]);
+  // Setup headers - I used the same headers from Firefox version 2.0.0.6
+  // below was split up because php.net said the line was too long. :/
+  $header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,";
+  $header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
+  $header[] = "Cache-Control: max-age=0";
+  $header[] = "Connection: keep-alive";
+  $header[] = "Keep-Alive: 300";
+  $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+  $header[] = "Accept-Language: en-us,en;q=0.5";
+  $header[] = "Pragma: "; // browsers keep this blank.
 
-//execute post
-$result = curl_exec($ch);
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_USERAGENT, 'Googlebot/2.1 (+http://www.google.com/bot.html)');
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+  curl_setopt($curl, CURLOPT_REFERER, 'http://www.google.com');
+  curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate');
+  curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_TIMEOUT, 10);
 
-//close connection
-curl_close($ch);
-echo $result;
-exit;
+  $html = curl_exec($curl); // execute the curl command
+  curl_close($curl); // close the connection
+
+  return $html; // and finally, return $html
+}
+
+// uses the function and displays the text off the website
+$text = disguise_curl($url);
+echo $text;
 ?>

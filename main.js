bf50d4f0b5c5ef52d899d340323b23e3395d0e78
xvarmkorv2 <49081442+xvarmkorv2@users.noreply.github.com>
2021-12-24 16:46:13 -0500

    update 57

--- main.js
+++ main.js
@@ -14357,8 +14357,9 @@ Game.Launch=function()
 			l('compactCookies').innerHTML=str;
 			Timer.track('cookie amount');
 			
-			for (var me of Game.Objects)
+			for (var i in Game.Objects)
 			{
+				var me = Game.Objects[i]
 				if (me.onMinigame && me.minigame.draw && !me.muted && !Game.onMenu) me.minigame.draw();
 			}
 			Timer.track('draw minigames');
@@ -14367,8 +14368,9 @@ Game.Launch=function()
 			{
 				//if (Game.prefs.monospace) {l('cookies').className='title monospace';} else {l('cookies').className='title';}
 				var lastLocked=0;
-				for (var me of Game.Objects)
+				for (var i in Game.Objects)
 				{
+					var me = Game.Objects[i]
 					//make products full-opacity if we can buy them
 					var classes='product';
 					var price=me.bulkPrice;
@@ -14381,8 +14383,9 @@ Game.Launch=function()
 				
 				//make upgrades full-opacity if we can buy them
 				var lastPrice=0;
-				for (var me of Game.UpgradesInStore)
+				for (var i in Game.UpgradesInStore)
 				{
+					var me = Game.UpgradesInStore[i]
 					if (!me.bought)
 					{
 						var price=me.getPrice();

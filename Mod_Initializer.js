7ef3ca87b62345f11616b413a519b25c97547c37
xvarmkorv2 <49081442+xvarmkorv2@users.noreply.github.com>
2022-01-05 18:11:34 -0500

    update 79

--- Mod_Initializer.js
+++ Mod_Initializer.js
@@ -2,8 +2,8 @@ var waitForGame = function waitForGame() {
     if (typeof Game === 'object' && Game.ready) {
         Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
         Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
-        Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
-        //Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
+        //Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
+        Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
     } else setTimeout(waitForGame, 100)
 }
 setTimeout(waitForGame)

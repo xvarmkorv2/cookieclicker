653ac3a06011af4c3a867514c4c57713eaef5343
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-10-18 12:42:24 -0400

    Update Mod_Initializer.js

--- Mod_Initializer.js
+++ Mod_Initializer.js
@@ -4,7 +4,7 @@ var waitForGame = function waitForGame() {
         Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
         //Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
         Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
-        //Game.LoadMod('CookiStocker.js')
+        //Game.LoadMod('https://xvarmkorv2.github.io/cookieclicker/CookiStocker.js')
     } else setTimeout(waitForGame, 100)
 }
 setTimeout(waitForGame)

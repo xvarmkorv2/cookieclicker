8e87b100e1afab706514d297c1cc7c39a09f42fd
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-10-21 08:06:49 -0400

    stuff

--- main.js
+++ main.js
@@ -553,7 +553,19 @@ Game.Launch = function () {
 	else if (day >= 349 + leap && day <= 365 + leap) Game.baseSeason = 'christmas';
 	else {
 		//easter is a pain goddamn
-		var easterDay = function (Y) { var C = Math.floor(Y / 100); var N = Y - 19 * Math.floor(Y / 19); var K = Math.floor((C - 17) / 25); var I = C - Math.floor(C / 4) - Math.floor((C - K) / 3) + 19 * N + 15; I = I - 30 * Math.floor((I / 30)); I = I - Math.floor(I / 28) * (1 - Math.floor(I / 28) * Math.floor(29 / (I + 1)) * Math.floor((21 - N) / 11)); var J = Y + Math.floor(Y / 4) + I + 2 - C + Math.floor(C / 4); J = J - 7 * Math.floor(J / 7); var L = I - J; var M = 3 + Math.floor((L + 40) / 44); var D = L + 28 - 31 * Math.floor(M / 4); return new Date(Y, M - 1, D); }(year);
+		var easterDay = function (Y) { 
+			var C = Math.floor(Y / 100); 
+			var N = Y - 19 * Math.floor(Y / 19); 
+			var K = Math.floor((C - 17) / 25); 
+			var I = C - Math.floor(C / 4) - Math.floor((C - K) / 3) + 19 * N + 15; 
+			I = I - 30 * Math.floor((I / 30)); 
+			I = I - Math.floor(I / 28) * (1 - Math.floor(I / 28) * Math.floor(29 / (I + 1)) * Math.floor((21 - N) / 11)); 
+			var J = Y + Math.floor(Y / 4) + I + 2 - C + Math.floor(C / 4); J = J - 7 * Math.floor(J / 7); 
+			var L = I - J; 
+			var M = 3 + Math.floor((L + 40) / 44); 
+			var D = L + 28 - 31 * Math.floor(M / 4); 
+			return new Date(Y, M - 1, D); 
+		}(year);
 		easterDay = Math.floor((easterDay - new Date(easterDay.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24));
 		if (day >= easterDay - 7 && day <= easterDay) Game.baseSeason = 'easter';
 	}

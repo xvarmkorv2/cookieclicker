67d86831d93b85ced420cad1d4575151fc36aaf2
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-10-28 07:50:19 -0400

    test

--- main.js
+++ main.js
@@ -1881,7 +1881,7 @@ Game.Launch = function () {
 		}
 		Game.ImportSave = function () {
 			Game.Prompt('<h3>Import save</h3><div class="block">Please paste in the code that was given to you on save export.</div><div class="block"><textarea id="textareaPrompt" style="width:100%;height:128px;"></textarea></div>', [['Load', 'if (l(\'textareaPrompt\').value.length>0) {Game.ImportSaveCode(l(\'textareaPrompt\').value);Game.ClosePrompt();}'], 'Nevermind']);//prompt('Please paste in the text that was given to you on save export.','');
-			l('textareaPrompt').focus();
+			l('textareaPrompt').focus();xx
 		}
 		Game.ImportSaveCode = function (save) {
 			if (save && save != '') Game.LoadSave(save);

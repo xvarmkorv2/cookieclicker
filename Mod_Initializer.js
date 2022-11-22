f1b0a5164d940436544d36689262e34c2adfd635
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-11-22 17:54:14 -0500

    test

--- Mod_Initializer.js
+++ Mod_Initializer.js
@@ -3,6 +3,7 @@ var waitForGame = function waitForGame() {
     if (typeof Game === 'object' && Game.ready) {
         Game.LoadMod('https://xvarmkorv2.github.io/cookieclicker/CCSE.js', () => {
             CCSE.ReplaceBuildings = () => { requestAnimationFrame(CCSE.playlist[CCSE.track++]); }
+            CCSE.ReplaceBuildingsFinish = () => {}
         });
         //Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
         Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12')

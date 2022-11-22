e4a92a99889c5ad28ddfb7816c93a81c76addce3
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-11-22 10:53:46 -0500

    test

--- Mod_Initializer.js
+++ Mod_Initializer.js
@@ -5,6 +5,7 @@ var waitForGame = function waitForGame() {
         //Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
         Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12')
         Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16')
+        Game.LoadMod('https://klattmose.github.io/CookieClicker/FortuneCookie.js?v=2.8')
         //Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
         if (EnableCookiStocker) {
             Game.LoadMod('https://xvarmkorv2.github.io/cookieclicker/CookiStocker.js')

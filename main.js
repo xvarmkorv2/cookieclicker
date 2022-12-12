60020f2a362f6e7d4093803c5401408f9d30fe07
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-12-11 22:13:36 -0500

    test

--- main.js
+++ main.js
@@ -2551,8 +2551,10 @@ Game.Launch=function () {
 		Game.heralds=0;
 
 		Game.GrabData=function () {
-			if (!App) ajax('https://cookie-clicker-thing.herokuapp.com/patreon/grab.php', Game.GrabDataResponse);
-			else App.grabData(function (res) {
+			if (!App) {
+				//ajax('https://cookie-clicker-thing.herokuapp.com/patreon/grab.php', Game.GrabDataResponse);
+				ajax('https://orteil.dashnet.org/patreon/grab.php', Game.GrabDataResponse);
+			} else App.grabData(function (res) {
 				Game.heralds=res ? (res.playersN || 1) : 1;
 				Game.heralds=Math.max(0, Math.min(100, Math.ceil(Game.heralds / 100 * 100) / 100));
 				l('heraldsAmount').textContent=Math.floor(Game.heralds);

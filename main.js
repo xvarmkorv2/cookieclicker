b74c515cb72661bcc40238ac93a3f30cdabcccfd
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-12-14 11:27:46 -0500

    test

--- main.js
+++ main.js
@@ -14865,10 +14865,10 @@ Game.Launch=function () {
 
 			//these are kinda fun
 			if (Game.ascensionMode==2 && !Game.promptOn && Game.Scroll!=0 && Game.ScrollFrames++==3) {
+				Game.ScrollFrames=0;
 				if (Game.BigCookieState==1) {
 					Game.BigCookieState=2;
 				} else if (Game.BigCookieState==2) {
-					Game.ScrollFrames=0;
 					Game.BigCookieState=1;
 					Game.ClickCookie();
 				}

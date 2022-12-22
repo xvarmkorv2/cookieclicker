15d57bf303e7d9ed7708fc17d7684f4874fad32f
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-12-22 13:51:19 -0500

    test

--- Mod_Initializer.js
+++ Mod_Initializer.js
@@ -19,22 +19,26 @@ Mods.LoadFolder = function (folder, lang, callback) {
 
 Mods.LoadMods = function (launch) {
     let callbackN = 0
-    let callbackN2 = 5
+    let callbackN2 = 0
     let callbackCheck = () => {
         callbackN++
         if (callbackN == callbackN2) {
             launch()
         }
     }
-    Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js', callbackCheck);
-    //Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
-    Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12', callbackCheck)
-    Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16', callbackCheck)
-    Game.LoadMod('https://klattmose.github.io/CookieClicker/FortuneCookie.js?v=2.8', callbackCheck)
-    Mods.LoadFolder('Mods/ameliaWatson', true, callbackCheck)
+    let modLoadCheck = (func, path) => {
+        callbackN2++
+        func(path, callbackCheck)
+    }
+    modLoadCheck(Game.LoadMod, 'https://klattmose.github.io/CookieClicker/CCSE.js')
+    //modLoadCheck(Game.LoadMod, 'https://staticvariablejames.github.io/InsugarTrading/dist/main.js')
+    modLoadCheck(Game.LoadMod, 'https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12')
+    modLoadCheck(Game.LoadMod, 'https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16')
+    modLoadCheck(Game.LoadMod, 'https://klattmose.github.io/CookieClicker/FortuneCookie.js?v=2.8')
+    modLoadCheck(Mods.LoadFolder, 'Mods/ameliaWatson')
     //Mods.LoadFolder('Mods/evenMoreBackgrounds', false)
     //Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
     if (EnableCookiStocker) {
-        Game.LoadMod('https://xvarmkorv2.github.io/cookieclicker/CookiStocker.js')
+        modLoadCheck(Game.LoadMod, '/CookiStocker.js')
     }
 }

e2418370ba039c5dd06d48d279692fc742f651d2
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-12-22 11:18:46 -0500

    test

--- Mod_Initializer.js
+++ Mod_Initializer.js
@@ -1,18 +1,20 @@
 const EnableCookiStocker = false
-var waitForGame = function waitForGame() {
-    if (typeof Game === 'object') {
-        Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
-        //Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
-        Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12')
-        Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16')
-        Game.LoadMod('https://klattmose.github.io/CookieClicker/FortuneCookie.js?v=2.8')
-        Game.LoadMod('Mods/ameliaWatson/lang.js')
-        Game.LoadMod('Mods/ameliaWatson/main.js')
-		Game.LoadMod('Mods/evenMoreBackgrounds/main.js')
-        //Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
-        if (EnableCookiStocker) {
-            Game.LoadMod('https://xvarmkorv2.github.io/cookieclicker/CookiStocker.js')
-        }
-    } else setTimeout(waitForGame, 100)
+
+var LoadModFolder = function (folder, lang) {
+    if (lang) {
+        Game.LoadMod(folder + '/lang.js')
+    }
+    Game.LoadMod(folder + '/main.js')
 }
-setTimeout(waitForGame)
+
+Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+//Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
+Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12')
+Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16')
+Game.LoadMod('https://klattmose.github.io/CookieClicker/FortuneCookie.js?v=2.8')
+LoadModFolder('Mods/ameliaWatson', true)
+LoadModFolder('Mods/evenMoreBackgrounds', false)
+//Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
+if (EnableCookiStocker) {
+    Game.LoadMod('https://xvarmkorv2.github.io/cookieclicker/CookiStocker.js')
+}

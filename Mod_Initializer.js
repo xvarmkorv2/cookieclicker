0898cac0c85805a4663d8ba912fec0acc8ceaa8b
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-12-22 11:24:03 -0500

    test

--- Mod_Initializer.js
+++ Mod_Initializer.js
@@ -7,14 +7,19 @@ var LoadModFolder = function (folder, lang) {
     Game.LoadMod(folder + '/main.js')
 }
 
-Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
-//Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
-Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12')
-Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16')
-Game.LoadMod('https://klattmose.github.io/CookieClicker/FortuneCookie.js?v=2.8')
-LoadModFolder('Mods/ameliaWatson', true)
-LoadModFolder('Mods/evenMoreBackgrounds', false)
-//Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
-if (EnableCookiStocker) {
-    Game.LoadMod('https://xvarmkorv2.github.io/cookieclicker/CookiStocker.js')
+var waitForGame = function waitForGame() {
+    if (typeof Game === 'object' && Game.LoadMod) {
+        Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+        //Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
+        Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12')
+        Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16')
+        Game.LoadMod('https://klattmose.github.io/CookieClicker/FortuneCookie.js?v=2.8')
+        LoadModFolder('Mods/ameliaWatson', true)
+        LoadModFolder('Mods/evenMoreBackgrounds', false)
+        //Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
+        if (EnableCookiStocker) {
+            Game.LoadMod('https://xvarmkorv2.github.io/cookieclicker/CookiStocker.js')
+        }
+    } else setTimeout(waitForGame, 100)
 }
+setTimeout(waitForGame)

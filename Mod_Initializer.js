c8498aef3fefe1761118f0575108d6222199d2b3
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2022-12-22 12:38:22 -0500

    test

--- Mod_Initializer.js
+++ Mod_Initializer.js
@@ -3,26 +3,38 @@ const EnableCookiStocker = false
 
 Mods = {}
 
-Mods.LoadFolder = function (folder, lang) {
-    if (lang) {
-        Game.LoadMod(folder + '/lang.js')
+Mods.LoadFolder = function (folder, lang, callback) {
+    let callbackN=0
+    let callbackCheck = ()=>{
+        callbackN++
+        if (callbackN == lang ? 1 : 2){
+            callback()
+        }
     }
-    Game.LoadMod(folder + '/main.js')
+    if (lang) {
+        Game.LoadMod(folder + '/lang.js', callbackCheck)
+    }
+    Game.LoadMod(folder + '/main.js', callbackCheck)
 }
 
 Mods.LoadMods = function (launch) {
-    Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+    let callbackN = 0
+    let callbackN2 = 5
+    let callbackCheck = () => {
+        callbackN++
+        if (callbackN == callbackN2) {
+            launch()
+        }
+    }
+    Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js', callbackCheck);
     //Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/dist/main.js');
-    Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12')
-    Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16')
-    Game.LoadMod('https://klattmose.github.io/CookieClicker/FortuneCookie.js?v=2.8')
-    Mods.LoadFolder('Mods/ameliaWatson', true)
-    //Mods.LoadFolder ('Mods/evenMoreBackgrounds', false)
+    Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.12', callbackCheck)
+    Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js?v=3.16', callbackCheck)
+    Game.LoadMod('https://klattmose.github.io/CookieClicker/FortuneCookie.js?v=2.8', callbackCheck)
+    Mods.LoadFolder('Mods/ameliaWatson', true, callbackCheck)
+    //Mods.LoadFolder('Mods/evenMoreBackgrounds', false)
     //Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
     if (EnableCookiStocker) {
         Game.LoadMod('https://xvarmkorv2.github.io/cookieclicker/CookiStocker.js')
     }
-    setTimeout(() => {
-        launch()
-    }, 1000);
 }

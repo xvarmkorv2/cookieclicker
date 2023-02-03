f704af26a0f68bd68187a49e3e585e6c8bc91069
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2023-02-02 19:51:53 -0500

    test

--- Mods/ameliaWatson/main.js
+++ Mods/ameliaWatson/main.js
@@ -51,65 +51,92 @@ var AmeliaWatson = {
 	},
 	check: function () {
 		let watsonicons = this.dir + "/watsoniconfull.png";
+		let shouldRebuild = false
 		Game.Objects['Time machine'].displayName = '<span style="font-size:70%;letter-spacing:-1px;position:relative;bottom:4px;">Amelia Watson</span>';//shrink
-		Game.Objects['Time machine'].pic = [0, 0, watsonicons];
+		if (Game.Objects['Time machine'].pic[2] != watsonicons) shouldRebuild = true
 
-		// check out the wiki for upgrade lists or the game's main.js file! thanks orteil! i love ctrl-f !!
+		if (!shouldRebuild) {
+			const upgrades = ['Flux capacitors', 'Time paradox resolver', 'Quantum conundrum', 'Causality enforcer',
+				'Yestermorrow comparators', 'Far future enactment', 'Great loop hypothesis', 'Cookietopian moments of maybe',
+				'Second seconds', 'Additional clock hands', 'Nostalgia', 'Split seconds', 'Patience abolished',
+				'Relativistic parsec-skipping', 'Primeval glow', 'Fortune #012', 'D&eacute;j&agrave; vu']
 
-		/* UPGRADES */
+			for (var i in upgrades) { if (Game.Upgrades[upgrades[i]].icon[2] != watsonicons) shouldRebuild = true; break }
+		}
 
-		// Tier
-		Game.Upgrades['Flux capacitors'].icon = [0, 0, watsonicons];
-		Game.Upgrades['Time paradox resolver'].icon = [1, 0, watsonicons];
-		Game.Upgrades['Quantum conundrum'].icon = [2, 0, watsonicons];
-		Game.Upgrades['Causality enforcer'].icon = [3, 0, watsonicons];
-		Game.Upgrades['Yestermorrow comparators'].icon = [4, 0, watsonicons];
-		Game.Upgrades['Far future enactment'].icon = [5, 0, watsonicons];
-		Game.Upgrades['Great loop hypothesis'].icon = [6, 0, watsonicons];
-		Game.Upgrades['Cookietopian moments of maybe'].icon = [7, 0, watsonicons];
-		Game.Upgrades['Second seconds'].icon = [8, 0, watsonicons];
-		Game.Upgrades['Additional clock hands'].icon = [9, 0, watsonicons];
-		Game.Upgrades['Nostalgia'].icon = [18, 0, watsonicons];
-		Game.Upgrades['Split seconds'].icon = [20, 0, watsonicons];
-		Game.Upgrades['Patience abolished'].icon = [21, 0, watsonicons];
+		if (!shouldRebuild) {
+			const achievements = ['Time warp', 'Alternate timeline', 'Rewriting history', 'Time duke',
+				'Forever and ever', 'Heat death', 'cookie clicker forever and forever a hundred years cookie clicker, all day long forever, forever a hundred times, over and over cookie clicker adventures dot com',
+				'Way back then', 'Invited to yesterday\'s party', 'Groundhog day', 'The years start coming',
+				'Caveman to cosmos', 'Back already?', 'Spacetime jigamaroo', 'Be kind, rewind', 'The long now']
 
-		// Synergy
-		Game.Upgrades['Relativistic parsec-skipping'].icon = [10, 0, watsonicons];
-		Game.Upgrades['Primeval glow'].icon = [19, 0, watsonicons];
+			for (var i in achievements) { if (Game.Upgrades[Achievements[i]].icon[2] != watsonicons) shouldRebuild = true; break }
+		}
 
-		// Fortune
-		Game.Upgrades['Fortune #012'].icon = [22, 0, watsonicons];
+		if (!shouldRebuild) {
+			if (Game.dragonAuras[13].pic[2] != watsonicons) shouldRebuild = true
+		}
 
-		/* ACHIEVOS */
+		if (shouldRebuild) {
+			Game.Objects['Time machine'].pic = [0, 0, watsonicons];
 
-		// Tier
-		Game.Achievements['Time warp'].icon = [0, 0, watsonicons];
-		Game.Achievements['Alternate timeline'].icon = [1, 0, watsonicons];
-		Game.Achievements['Rewriting history'].icon = [2, 0, watsonicons];
-		Game.Achievements['Time duke'].icon = [3, 0, watsonicons];
-		Game.Achievements['Forever and ever'].icon = [4, 0, watsonicons];
-		Game.Achievements['Heat death'].icon = [5, 0, watsonicons];
-		Game.Achievements['cookie clicker forever and forever a hundred years cookie clicker, all day long forever, forever a hundred times, over and over cookie clicker adventures dot com'].icon = [6, 0, watsonicons];
-		Game.Achievements['Way back then'].icon = [7, 0, watsonicons];
-		Game.Achievements['Invited to yesterday\'s party'].icon = [8, 0, watsonicons];
-		Game.Achievements['Groundhog day'].icon = [9, 0, watsonicons];
-		Game.Achievements['The years start coming'].icon = [18, 0, watsonicons];
-		Game.Achievements['Caveman to cosmos'].icon = [20, 0, watsonicons];
-		Game.Achievements['Back already?'].icon = [21, 0, watsonicons];
+			// check out the wiki for upgrade lists or the game's main.js file! thanks orteil! i love ctrl-f !!
 
-		// Statue
-		Game.Achievements['Spacetime jigamaroo'].icon = [12, 0, watsonicons];
-		Game.Achievements['Be kind, rewind'].icon = [13, 0, watsonicons];
-		Game.Achievements['D&eacute;j&agrave; vu'].icon = [14, 0, watsonicons];
+			/* UPGRADES */
 
-		// Level 10 Sugar Lump
-		Game.Achievements['The long now'].icon = [16, 0, watsonicons];
+			// Tier
+			Game.Upgrades['Flux capacitors'].icon = [0, 0, watsonicons];
+			Game.Upgrades['Time paradox resolver'].icon = [1, 0, watsonicons];
+			Game.Upgrades['Quantum conundrum'].icon = [2, 0, watsonicons];
+			Game.Upgrades['Causality enforcer'].icon = [3, 0, watsonicons];
+			Game.Upgrades['Yestermorrow comparators'].icon = [4, 0, watsonicons];
+			Game.Upgrades['Far future enactment'].icon = [5, 0, watsonicons];
+			Game.Upgrades['Great loop hypothesis'].icon = [6, 0, watsonicons];
+			Game.Upgrades['Cookietopian moments of maybe'].icon = [7, 0, watsonicons];
+			Game.Upgrades['Second seconds'].icon = [8, 0, watsonicons];
+			Game.Upgrades['Additional clock hands'].icon = [9, 0, watsonicons];
+			Game.Upgrades['Nostalgia'].icon = [18, 0, watsonicons];
+			Game.Upgrades['Split seconds'].icon = [20, 0, watsonicons];
+			Game.Upgrades['Patience abolished'].icon = [21, 0, watsonicons];
+
+			// Synergy
+			Game.Upgrades['Relativistic parsec-skipping'].icon = [10, 0, watsonicons];
+			Game.Upgrades['Primeval glow'].icon = [19, 0, watsonicons];
+
+			// Fortune
+			Game.Upgrades['Fortune #012'].icon = [22, 0, watsonicons];
+
+			/* ACHIEVOS */
+
+			// Tier
+			Game.Achievements['Time warp'].icon = [0, 0, watsonicons];
+			Game.Achievements['Alternate timeline'].icon = [1, 0, watsonicons];
+			Game.Achievements['Rewriting history'].icon = [2, 0, watsonicons];
+			Game.Achievements['Time duke'].icon = [3, 0, watsonicons];
+			Game.Achievements['Forever and ever'].icon = [4, 0, watsonicons];
+			Game.Achievements['Heat death'].icon = [5, 0, watsonicons];
+			Game.Achievements['cookie clicker forever and forever a hundred years cookie clicker, all day long forever, forever a hundred times, over and over cookie clicker adventures dot com'].icon = [6, 0, watsonicons];
+			Game.Achievements['Way back then'].icon = [7, 0, watsonicons];
+			Game.Achievements['Invited to yesterday\'s party'].icon = [8, 0, watsonicons];
+			Game.Achievements['Groundhog day'].icon = [9, 0, watsonicons];
+			Game.Achievements['The years start coming'].icon = [18, 0, watsonicons];
+			Game.Achievements['Caveman to cosmos'].icon = [20, 0, watsonicons];
+			Game.Achievements['Back already?'].icon = [21, 0, watsonicons];
+
+			// Statue
+			Game.Achievements['Spacetime jigamaroo'].icon = [12, 0, watsonicons];
+			Game.Achievements['Be kind, rewind'].icon = [13, 0, watsonicons];
+			Game.Achievements['D&eacute;j&agrave; vu'].icon = [14, 0, watsonicons];
+
+			// Level 10 Sugar Lump
+			Game.Achievements['The long now'].icon = [16, 0, watsonicons];
 
 
-		/* DRAGON (DEEZ NUTS) im literally bull[$!$!]ing my way thru here */
+			/* DRAGON (DEEZ NUTS) im literally bull[$!$!]ing my way thru here */
 
-		Game.dragonAuras[13].pic = [15, 0, watsonicons];
-		//IT WORKS LETS GOOOOOOO I AM AN ARRAY GOOOOOD
+			Game.dragonAuras[13].pic = [15, 0, watsonicons];
+			//IT WORKS LETS GOOOOOOO I AM AN ARRAY GOOOOOD
+		}
 	}
 }
 AmeliaWatson.launch = () => { Game.registerMod("despacito AmeliaWatson", AmeliaWatson); }

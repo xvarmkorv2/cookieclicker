7423e69ae26abcc294fe2bf07c67886f2b71b0ab
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2023-02-22 21:26:35 -0500

    test

--- Mods/MoreHeavenlyUpgrades/main.js
+++ Mods/MoreHeavenlyUpgrades/main.js
@@ -7,7 +7,7 @@ MoreHeavenlyUpgrades.launch = function() {
     MoreHeavenlyUpgrades.init = function() {
         MoreHeavenlyUpgrades.isLoaded = 1;
         Game.Notify('More heavenly upgrades loaded', '', [19, 7], 6);
-        const WatsonIcons = Game.Mods["despacito AmeliaWatson"] !== undefined ? Game.Mods["despacito AmeliaWatson"].dir + "/watsoniconfull.png" : 0
+        const WatsonIcons = Game.mods["despacito AmeliaWatson"] !== undefined ? Game.mods["despacito AmeliaWatson"].dir + "/watsoniconfull.png" : 0
 
         //-----------------------------------permanent upgrade slots maybe
 

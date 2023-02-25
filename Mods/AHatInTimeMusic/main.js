51247b5c9a4e7c6d440a542e5fca5feb457be1de
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2023-02-25 17:55:51 -0500

    test

--- Mods/AHatInTimeMusic/main.js
+++ Mods/AHatInTimeMusic/main.js
@@ -3,10 +3,12 @@ Game.registerMod("A Hat in Time Music", {
 		Music.addTrack('Title Screen', 'Pascal Michael Stiefel', this.dir + '/TitleScreen.mp3');
 		Music.addTrack('File Select', 'Pascal Michael Stiefel', this.dir + '/FileSelect.mp3');
 		Music.addTrack('Your Contract has expired', 'Pascal Michael Stiefel', this.dir + '/YourContractHasExpired.mp3');
+		Music.addTrack('Roll the Slots', 'Pascal Michael Stiefel', this.dir + '/RollTheSlots.mp3');
 		Game.jukebox.tracks.push(
 			'Title Screen',
 			'File Select',
-			'Your Contract has expired'
+			'Your Contract has expired',
+			'Roll the Slots'
 		)
     }
 })

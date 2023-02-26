2940ba620e113ff5e217f0ec1e8c1b82d88a113b
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2023-02-25 19:19:35 -0500

    tset

--- Mods/AHatInTimeMusic/main.js
+++ Mods/AHatInTimeMusic/main.js
@@ -1,5 +1,5 @@
 Game.registerMod("A Hat in Time Music", {
-    init: function () {
+	init: function () {
 		Music.addTrack('Title Screen', 'Pascal Michael Stiefel', this.dir + '/02TitleScreen.mp3');
 		Music.addTrack('File Select', 'Pascal Michael Stiefel', this.dir + '/03FileSelect.mp3');
 		Music.addTrack('Welcome to Mafia Town', 'Pascal Michael Stiefel', this.dir + '/06WelcometoMafiaTown.mp3');
@@ -7,9 +7,9 @@ Game.registerMod("A Hat in Time Music", {
 		Music.addTrack('Train Rush', 'Pascal Michael Stiefel', this.dir + '/33TrainRush.mp3');
 		Music.addTrack('Dead Bird Studio Basment', 'Pascal Michael Stiefel', this.dir + '/38DeadBirdStudioBasement.mp3');
 		Music.addTrack('Dead Bird Studio is Closed', 'Pascal Michael Stiefel', this.dir + '/39DeadBirdStudioIsClosed.mp3');
-		Music.addTrack('The Battle of Award 42', 'Pascal Michael Stiefel', this.dir + '/39TheBattleofAward42.mp3');
-		Music.addTrack('Heart To Heart', 'Pascal Michael Stiefel', this.dir + '/40TheBattleofAward42.mp3');
-		Music.addTrack('Your Contract has expired', 'Pascal Michael Stiefel', this.dir + '/41HeartToHeart.mp3');
+		Music.addTrack('The Battle of Award 42', 'Pascal Michael Stiefel', this.dir + '/40TheBattleofAward42.mp3');
+		Music.addTrack('Heart To Heart', 'Pascal Michael Stiefel', this.dir + '/41HeartToHeart.mp3');
+		Music.addTrack('Your Contract has expired', 'Pascal Michael Stiefel', this.dir + '/55YourContractHasExpired.mp3');
 		Music.addTrack('Roll the Slots', 'Pascal Michael Stiefel', this.dir + '/72RollTheSlots.mp3');
 		Game.jukebox.tracks.push(
 			'Title Screen',
@@ -24,5 +24,5 @@ Game.registerMod("A Hat in Time Music", {
 			'Your Contract has expired',
 			'Roll the Slots'
 		)
-    }
+	}
 })

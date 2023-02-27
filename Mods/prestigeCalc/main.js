cc534b603564828e217908524eb27966d14223a1
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2023-02-27 16:57:15 -0500

    test

--- Mods/prestigeCalc/main.js
+++ Mods/prestigeCalc/main.js
@@ -1,9 +1,13 @@
-Game.registerMod("prestigeCalc",{
+Game.registerMod("prestigeCalc", {
 	init: () => {
 		//===============================PrestigeCount==========================================
 		let ascendNowToGet = 0;
 		let prestigeLvlAfter = () => Math.floor(Game.prestige + ascendNowToGet);
 		let prestigeLvlAfterStr = () => (Beautify(prestigeLvlAfter(), 1)).replace(' ', '&nbsp;');
+
+		// Find the parent node and the reference node to prepend the new element to
+		let prestigeparent = document.querySelector('#legacyButton'),
+			reference = document.querySelector('#ascendNumber');
 
 		let prestigeCount = document.createElement('div');
 		prestigeCount.innerHTML = prestigeLvlAfterStr();
@@ -11,16 +15,12 @@ Game.registerMod("prestigeCalc",{
 		prestigeCount.setAttribute('id', 'prestigeCalc');
 
 		prestigeCount.style.position = 'absolute';
-		prestigeCount.style.right = '300%';
+		prestigeCount.style.right = reference.clientWidth + 115 + 25 + 'px';
 		prestigeCount.style.top = '20px';
 		prestigeCount.style.fontSize = '12px';
 		prestigeCount.style.fontWeight = 'bold';
 		prestigeCount.style.fontFamily = 'Georgia';
 		prestigeCount.style.color = '#999';
-
-		// Find the parent node and the reference node to prepend the new element to
-		let prestigeparent = document.querySelector('#legacyButton'),
-			reference = document.querySelector('#ascendNumber');
 
 		prestigeparent.insertBefore(prestigeCount, reference);
 
@@ -29,14 +29,14 @@ Game.registerMod("prestigeCalc",{
 		//===============================SevenCount=============================================
 		let sevenCount = () => (prestigeLvlAfter().toString().match(/7/g) || []).length;
 		let sevenCountStr = () => (sevenCount());
-		
+
 		let sevenCounter = document.createElement('div');
 		sevenCounter.innerHTML = sevenCountStr();
 		sevenCounter.className = "roundedPanel";
 		sevenCounter.setAttribute('id', 'sevenCounter');
 
 		sevenCounter.style.position = 'absolute';
-		sevenCounter.style.right = '300%';
+		sevenCounter.style.right = reference.clientWidth + 115 + 25 + 'px';
 		sevenCounter.style.top = '0';
 		sevenCounter.style.fontSize = '12px';
 		sevenCounter.style.fontWeight = 'bold';
@@ -49,12 +49,15 @@ Game.registerMod("prestigeCalc",{
 		let logic = () => {
 			if (prestigeLvlAfter() > 0) {
 				ascendNowToGet = Math.floor(Game.HowMuchPrestige(Game.cookiesReset + Game.cookiesEarned)) - Math.floor(Game.HowMuchPrestige(Game.cookiesReset));
+
 				prestigeCount.innerHTML = prestigeLvlAfterStr();
 				prestigeCount.style.display = 'block';
+				prestigeCount.style.right = reference.clientWidth + 115 + 25 + 'px';
 
 				sevenCounter.innerHTML = sevenCountStr();
 				sevenCounter.style.display = 'block';
-				
+				sevenCounter.style.right = reference.clientWidth + 115 + 25 + 'px';
+
 			} else {
 				prestigeCount.style.display = 'none';
 				sevenCounter.style.display = 'none';

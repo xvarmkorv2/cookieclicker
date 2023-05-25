a95c4723b40085106ce7db12195528b514cd5d84
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2023-05-25 11:25:38 -0400

    Test

--- main.js
+++ main.js
@@ -14532,6 +14532,7 @@ new Game.Upgrade('Wrapping paper',loc("You may now send and receive gifts with o
 		
 		Game.wrinklerHP=2.1;
 		Game.wrinklers=[];
+        Game.wrinklerLimit=14;
 		for (var i=0;i<Game.wrinklerLimit;i++)
 		{
 			Game.wrinklers.push({id:parseInt(i),close:0,sucked:0,phase:0,x:0,y:0,r:0,hurt:0,hp:Game.wrinklerHP,selected:0,type:0,clicks:0});

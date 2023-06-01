36a053670c54b871d36715aebf962d1b4abdfd26
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2023-05-31 21:08:04 -0400

    test

--- Mods/sexButton/main.js
+++ Mods/sexButton/main.js
@@ -5,19 +5,23 @@ function sex() {
 
 Game.registerMod("sex button",{
 	init:function() {
-
-		// Copy vanilla code for menu updating into a new function
-		var oldUpdateMenu = Game.UpdateMenu.bind({});
-
-		// Overwrite the old function with my code, but run the copied vanilla code as well, as to preserve functionality
-		Game.UpdateMenu = () => {
-			oldUpdateMenu();
-			if (Game.onMenu=='prefs') {
+		if (CCSE){
+			if(!Game.customOptionsMenu) Game.customOptionsMenu = [];
+			Game.customOptionsMenu.push(function () {
 				//document.getElementsByClassName('listing')[8].innerHTML += '<a class="option" onclick="sex()">Sex</a><label>(OMG!)</label><br>';
 				let listings = document.getElementsByClassName('listing');
 				listings[listings.length - 1].innerHTML += '<a class="option" onclick="sex()">Sex</a><label>(OMG!)</label><br>';
+			});
+		} else {
+			var menu = Game.UpdateMenu;
+			Game.UpdateMenu = function () {
+				menu();
+				if (Game.onMenu == 'prefs') {
+					//document.getElementsByClassName('listing')[8].innerHTML += '<a class="option" onclick="sex()">Sex</a><label>(OMG!)</label><br>';
+				let listings = document.getElementsByClassName('listing');
+				listings[listings.length - 1].innerHTML += '<a class="option" onclick="sex()">Sex</a><label>(OMG!)</label><br>';
+				}
 			}
 		}
-
 	}
 });

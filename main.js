37f945a5319dc85ec525e17fa263b1bcc2054aed
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2025-03-25 12:59:24 -0400

    test

--- main.js
+++ main.js
@@ -2774,6 +2774,7 @@ Game.Launch=function()
 			if (!App) {
 				ajax('https://api.allorigins.win/raw?url=https://orteil.dashnet.org/patreon/grab.php', Game.GrabDataResponse);
 				//ajax('https://cors.eu.org/https://orteil.dashnet.org/patreon/grab.php',Game.GrabDataResponse);
+				//ajax('https://corsproxy.io/https://orteil.dashnet.org/patreon/grab.php',Game.GrabDataResponse)
 				//ajax('patreon/grab.php',Game.GrabDataResponse);
 			} else App.grabData(function(res){
 				Game.heralds=res?(res.playersN||1):1;

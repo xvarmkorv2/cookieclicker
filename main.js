7c789b6223a640c0f38c6594db7c233cd7a50db8
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2025-06-13 19:39:03 -0400

    test

--- main.js
+++ main.js
@@ -2516,10 +2516,10 @@ Game.Launch=function()
 		Game.bakeryNameSet=function(what)
 		{
 			if (Game.bakeryName!=what && what=='harley')
-+			{//added by request as thanks to the very nice person who kept my twitter handle safe when my account got hacked by crypto scammers
-+				var rect=Game.bakeryNameL.getBounds();
-+				Game.Popup('Horse complex!',(rect.left+rect.right)/2,(rect.top+rect.bottom)/2-48);
-+			}
+			{//added by request as thanks to the very nice person who kept my twitter handle safe when my account got hacked by crypto scammers
+				var rect=Game.bakeryNameL.getBounds();
+				Game.Popup('Horse complex!',(rect.left+rect.right)/2,(rect.top+rect.bottom)/2-48);
+			}
 			try
 			{
 				var exp=new RegExp('[^\'\\-_0-9 \\p{L}]','gu');

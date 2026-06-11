f1392b09224ebeb3e2f3b0b8a715002480ea2d4a
Varmkorv Två <49081442+xvarmkorv2@users.noreply.github.com>
2026-06-11 18:54:00 -0400

    test

--- main.js
+++ main.js
@@ -14903,7 +14903,7 @@ Game.Launch = function () {
 						Game.Background.fillPattern(Pic(Game.bg), 0, 0, Game.Background.canvas.width, Game.Background.canvas.height, 512, 512, 0, 0);
 						if (Game.bgR > 0) {
 							Game.Background.globalAlpha = Game.bgR;
-							Game.Background.fillPattern(Pic(Game.bgFade), 0, 0, Game.Background.canvas.width, Game.Background.canvas.height, 512, 512, 0, 0);
+							Game.Background.fillPattern(Pic(Game.bgFade + '.jpg'), 0, 0, Game.Background.canvas.width, Game.Background.canvas.height, 512, 512, 0, 0);
 						}
 						Game.Background.globalAlpha = 1;
 						Game.Background.drawImage(Pic('shadedBordersSoft.png'), 0, 0, Game.Background.canvas.width, Game.Background.canvas.height);
@@ -16148,120 +16148,125 @@ Game.Launch = function () {
 	DRAW
 	=======================================================================================*/
 
-	Game.Draw = function () {
-		Timer.say('DRAW');
-		Game.DrawBackground(); Timer.track('end of background');
-
-		if (!Game.OnAscend) {
-
-			var str = Beautify(Math.round(Game.cookiesd));
+	Game.Draw=function()
+	{
+		Game.DrawBackground();Timer.track('end of background');
+		
+		if (!Game.OnAscend)
+		{
+			
+			var str=Beautify(Math.round(Game.cookiesd));
 			if (Game.cookiesd>=1000000)//dirty padding
 			{
-				var spacePos = str.indexOf(' ');
-				var dotPos = str.indexOf('.');
-				var add = '';
-				if (spacePos!=-1) {
-					if (dotPos==-1) add += '.000';
-					else {
-						if (spacePos - dotPos==2) add += '00';
-						if (spacePos - dotPos==3) add += '0';
+				var spacePos=str.indexOf(' ');
+				var dotPos=str.indexOf('.');
+				var add='';
+				if (spacePos!=-1)
+				{
+					if (dotPos==-1) add+='.000';
+					else
+					{
+						if (spacePos-dotPos==2) add+='00';
+						if (spacePos-dotPos==3) add+='0';
 					}
 				}
-				str = [str.slice(0, spacePos), add, str.slice(spacePos)].join('');
+				str=[str.slice(0,spacePos),add,str.slice(spacePos)].join('');
 			}
-
-			str = loc("%1 cookie", { n: Math.round(Game.cookiesd), b: str });
-			if (str.length > 14) str = str.replace(' ', '<br>');
-
-			if (Game.prefs.monospace) str = '<span class="monospace">' + str + '</span>';
-			str = str + '<div id="cookiesPerSecond"' + (Game.cpsSucked > 0 ? ' class="wrinkled"' : '') + '>' + loc("per second:") + ' ' + Beautify(Game.cookiesPs * (1 - Game.cpsSucked), 1) + '</div>';
-			l('cookies').innerHTML = str;
+			
+			str=loc("%1 cookie",{n:Math.round(Game.cookiesd),b:str});
+			if (str.length>14) str=str.replace(' ','<br>');
+			
+			if (Game.prefs.monospace) str='<span class="monospace">'+str+'</span>';
+			str=str+'<div id="cookiesPerSecond"'+(Game.cpsSucked>0?' class="wrinkled"':'')+'>'+loc("per second:")+' '+Beautify(Game.cookiesPs*(1-Game.cpsSucked),1)+'</div>';
+			l('cookies').innerHTML=str;
 			Timer.track('cookie amount');
-
-			for (var i = 0; i < Game.ObjectsById.length; i++) {
-				var me = Game.ObjectsById[i];
+			
+			for (var i=0;i<Game.ObjectsById.length;i++)
+			{
+				var me=Game.ObjectsById[i];
 				if (me.onMinigame && me.minigame.draw && !me.muted && !Game.onMenu) me.minigame.draw();
 			}
 			Timer.track('draw minigames');
-
-			if (Game.drawT % 5==0) {
+			
+			if (Game.drawT%5==0)
+			{
 				//if (Game.prefs.monospace) {l('cookies').className='title monospace';} else {l('cookies').className='title';}
-				var lastLocked = 0;
-				for (var i = 0; i < Game.ObjectsById.length; i++) {
-					var me = Game.ObjectsById[i];
-
+				var lastLocked=0;
+				for (var i=0;i<Game.ObjectsById.length;i++)
+				{
+					var me=Game.ObjectsById[i];
+					
 					//make products full-opacity if we can buy them
-					var classes = 'product';
-					var price = me.bulkPrice;
-					if (Game.cookiesEarned>=me.basePrice || me.bought > 0) { classes += ' unlocked'; lastLocked = 0; me.locked = 0; if (me.id==19) { Game.Win('Cookie Clicker'); } } else { classes += ' locked'; lastLocked++; me.locked = 1; }
-					if ((Game.buyMode==1 && Game.cookies>=price) || (Game.buyMode==-1 && me.amount > 0)) classes += ' enabled'; else classes += ' disabled';
-					if (lastLocked > 2) classes += ' toggledOff';
-					me.l.className = classes;
+					var classes='product';
+					var price=me.bulkPrice;
+					if (Game.cookiesEarned>=me.basePrice || me.bought>0) {classes+=' unlocked';lastLocked=0;me.locked=0;if (me.id==19){Game.Win('Cookie Clicker');}} else {classes+=' locked';lastLocked++;me.locked=1;}
+					if ((Game.buyMode==1 && Game.cookies>=price) || (Game.buyMode==-1 && me.amount>0)) classes+=' enabled'; else classes+=' disabled';
+					if (lastLocked>2) classes+=' toggledOff';
+					me.l.className=classes;
 					//if (me.id>0) {l('productName'+me.id).innerHTML=Beautify(me.storedTotalCps/Game.ObjectsById[me.id-1].storedTotalCps,2);}
 				}
-
+				
 				//make upgrades full-opacity if we can buy them
-				var lastPrice = 0;
-				for (var i = 0; i < Game.UpgradesInStore.length; i++) {
-					var me = Game.UpgradesInStore[i];
-					if (!me.bought) {
-						var price = me.getPrice();
-						var canBuy = me.canBuy();//(Game.cookies>=price);
-						var enabled = (l('upgrade' + i).className.indexOf('enabled') > -1);
-						if ((canBuy && !enabled) || (!canBuy && enabled)) Game.upgradesToRebuild = 1;
-						if (me.pool!='toggle') {
-							if (price < lastPrice) Game.upgradesToRebuild = 1;//is this upgrade less expensive than the previous one? trigger a refresh to sort it again
-							lastPrice = price;
-						}
-						lastPrice = price;
-					}
-					if (me.timerDisplay) {
-						var T = me.timerDisplay();
-						if (T!=-1) {
-							if (!l('upgradePieTimer' + i)) l('upgrade' + i).innerHTML = l('upgrade' + i).innerHTML + '<div class="pieTimer" id="upgradePieTimer' + i + '"></div>';
-							T = (T * 144) % 144;
-							l('upgradePieTimer' + i).style.backgroundPosition = (-Math.floor(T % 18)) * 48 + 'px ' + (-Math.floor(T / 18)) * 48 + 'px';
+				var lastPrice=0;
+				for (var i=0;i<Game.UpgradesInStore.length;i++)
+				{
+					var me=Game.UpgradesInStore[i];
+					if (!me.bought)
+					{
+						var price=me.getPrice();
+						var canBuy=me.canBuy();//(Game.cookies>=price);
+						var enabled=(l('upgrade'+i).className.indexOf('enabled')>-1);
+						if ((canBuy && !enabled) || (!canBuy && enabled)) Game.upgradesToRebuild=1;
+						if (me.pool!='toggle')
+						{
+							if (price<lastPrice) Game.upgradesToRebuild=1;//is this upgrade less expensive than the previous one? trigger a refresh to sort it again
+							lastPrice=price;
 						}
 					}
-
+					if (me.timerDisplay)
+					{
+						var T=me.timerDisplay();
+						if (T!=-1)
+						{
+							if (!l('upgradePieTimer'+i)) l('upgrade'+i).innerHTML=l('upgrade'+i).innerHTML+'<div class="pieTimer" id="upgradePieTimer'+i+'"></div>';
+							T=(T*144)%144;
+							l('upgradePieTimer'+i).style.backgroundPosition=(-Math.floor(T%18))*48+'px '+(-Math.floor(T/18))*48+'px';
+						}
+					}
+					
 					//if (me.canBuy()) l('upgrade'+i).className='crate upgrade enabled'; else l('upgrade'+i).className='crate upgrade disabled';
 				}
 			}
 			Timer.track('store');
-
+			
 			if (Game.PARTY)//i was bored and felt like messing with CSS
 			{
-				var pulse = Math.pow((Game.realT % 10) / 10, 0.5);
-				Game.l.style.filter = 'hue-rotate(' + ((Game.realT * 5) % 360) + 'deg) brightness(' + (150 - 50 * pulse) + '%)';
-				Game.l.style.webkitFilter = 'hue-rotate(' + ((Game.realT * 5) % 360) + 'deg) brightness(' + (150 - 50 * pulse) + '%)';
-				Game.l.style.transform = 'scale(' + (1.02 - 0.02 * pulse) + ',' + (1.02 - 0.02 * pulse) + ') rotate(' + (Math.sin(Game.realT * 0.5) * 0.5) + 'deg)';
-			} else {
-				Game.l.style.filter = '';
-				Game.l.style.webkitFilter = '';
-				Game.l.style.transform = '';
-				Game.wrapper.style.overflowX = '';
-				Game.wrapper.style.overflowY = '';
+				var pulse=Math.pow((Game.realT%10)/10,0.5);
+				Game.l.style.filter='hue-rotate('+((Game.realT*5)%360)+'deg) brightness('+(150-50*pulse)+'%)';
+				Game.l.style.webkitFilter='hue-rotate('+((Game.realT*5)%360)+'deg) brightness('+(150-50*pulse)+'%)';
+				Game.l.style.transform='scale('+(1.02-0.02*pulse)+','+(1.02-0.02*pulse)+') rotate('+(Math.sin(Game.realT*0.5)*0.5)+'deg)';
+				Game.wrapper.style.overflowX='hidden';
+				Game.wrapper.style.overflowY='hidden';
 			}
-
+			
 			Timer.clean();
-			if (Game.prefs.animate && (Game.prefs.fancy || (!Game.prefs.fancy && Game.drawT % 10==0)) && Game.AscendTimer==0 && Game.onMenu == '') Game.DrawBuildings(); Timer.track('buildings');
-
-			Game.textParticlesUpdate(); Timer.track('text particles');
+			if (Game.prefs.animate && ((Game.prefs.fancy && Game.drawT%1==0) || (!Game.prefs.fancy && Game.drawT%10==0)) && Game.AscendTimer==0 && Game.onMenu=='') Game.DrawBuildings();Timer.track('buildings');
+			
+			Game.textParticlesUpdate();Timer.track('text particles');
 		}
-
-		Game.NotesDraw(); Timer.track('notes');
-
+		
+		Game.NotesDraw();Timer.track('notes');
+		
 		Game.runModHook('draw');
-
+		
 		Game.drawT++;
-		Timer.say('END DRAW');
 		//if (Game.prefs.altDraw) requestAnimationFrame(Game.Draw);
 	}
 
 	/*=====================================================================================
 	MAIN LOOP
 	=======================================================================================*/
-	Game.Loop = function () {
+	Game.Loop=function () {
 		if (Game.timedout) return false;
 		var startTime = Date.now();//adapted from a mod called FPSTweaker
 		Timer.say('START');
